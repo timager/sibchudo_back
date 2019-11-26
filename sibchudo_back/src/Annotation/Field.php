c1990f819658dcd6a74b7837af8e6d220c7c5f40
timager <timger98@gmail.com>
2019-11-26 12:48:32 +0000

    database connector

--- sibchudo_back/src/Annotation/Field.php
+++ sibchudo_back/src/Annotation/Field.php
@@ -12,10 +12,8 @@ namespace App\Annotation;
 
 class Field
 {
-    private $type;
     private $name;
-    private $primary = false;
-    private $isNull = false;
+
 
     public function __construct(array $data)
     {
@@ -27,35 +25,10 @@ class Field
     /**
      * @return mixed
      */
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    /**
-     * @return mixed
-     */
     public function getName()
     {
         return $this->name;
     }
-
-    /**
-     * @return mixed
-     */
-    public function getPrimary()
-    {
-        return $this->primary;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isNull(): bool
-    {
-        return $this->isNull;
-    }
-
 
 
 }
