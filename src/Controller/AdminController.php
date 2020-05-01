2148753051e51b076aa5287d7527883bd022b75b
nik <nik_mak@bk.ru>
2020-05-02 00:50:55 +0300

    add security

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,21 +6,34 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController {
-    /**
-     * @Route("/api/login/check", name="login")
-     * @param Request $request
-     * @return Response
-     */
-    public function login(Request $request) {
-        $post = json_decode($request->getContent(), true);
-        if($post['username'] == "timager")
-            return new Response("",200);
-        else{
-            return new Response("",401);
-        }
-    }
+
+//    /**
+//     * @Route("/login", name="/login", methods={"POST"})
+//     */
+//    public function login(Request $request)
+//    {
+//        $user = $this->getUser();
+//
+//        return $this->json([
+//            'username' => $user->getUsername(),
+//            'roles' => $user->getRoles(),
+//        ]);
+//    }
+
+//    /**
+//     * @Route("/api/login/check", name="login")
+//     * @param Request $request
+//     * @return Response
+//     */
+//    public function login(Request $request) {
+//        $post = json_decode($request->getContent(), true);
+//        if($post['username'] == "timager")
+//            return new Response("",200);
+//        else{
+//            return new Response("",401);
+//        }
+//    }
 }
