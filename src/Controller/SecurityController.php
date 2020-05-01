b6a639905a6c45eab48fb96a92c799bd7b0bf4a9
nik <nik_mak@bk.ru>
2020-05-02 00:52:02 +0300

    security fix

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,7 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController {
     /**
-     * @Route("/login", name="login", methods={"POST"})
+     * @Route("/api/login", name="login", methods={"POST"})
      */
     public function login(Request $request) {
         $user = $this->getUser();
